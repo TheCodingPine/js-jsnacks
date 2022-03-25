634470064e9343cb5f787199163ca14c86cbeeed
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-25 17:00:22 +0100

    late setup

---
+++ jsnack1/script.js
@@ -0,0 +1,10 @@
+/*L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+
+let prompt 1
+let prompt 2
+rendili da string number
+
+if a > b printa a
+else printa b  || se sono uguali sarà comunque il maggiore*/
+
