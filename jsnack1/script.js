66f630b86668e7279da34efe5091647ea1f52cfa
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-25 17:06:02 +0100

    jsnack1 - complete

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -8,3 +8,13 @@ rendili da string number
 if a > b printa a
 else printa b  || se sono uguali sarà comunque il maggiore*/
 
+let a = prompt("Inserisci un numero");
+a = parseInt(a);
+let b = prompt("inseriscine un altro");
+b = parseInt(b);
+
+if (a > b) {
+    document.getElementById("output").innerHTML = a;
+}else{
+    document.getElementById("output").innerHTML = b;
+}
