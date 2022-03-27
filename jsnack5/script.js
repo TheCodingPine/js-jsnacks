0c09d20011cbf35f558eab0ce676ae015a14e036
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-27 18:34:24 +0200

    jsnack5 complete

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -1,3 +1,39 @@
-/*L’utente inserisce due numeri in successione, con due prompt.
-Il software stampa il maggiore.*/
+/*In un array sono contenuti i nomi degli invitati alla festa del grande
+Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
+no alla festa.
 
+Array di nomi
+
+prompta il nome
+
+if presente bene, altrimenti no
+
+
+*/
+
+
+let listaInvitati = ["Jim Browning", "Mario Falcone", "Anne Jhonson", "Reverendo Smith", "Arthur Shelby","Jhon Wayne"]; //più corretto let, const funziona comunque
+console.log(listaInvitati);
+
+let input = prompt ("Scrivi il tuo nome");
+console.log(input+" controlliamo se sei invitato");
+
+
+//dichiariamo la nuova mail
+let newUser;
+//input è nella array degli invitati?
+let trovato = false;
+
+for (let i=0; i < listaInvitati.length; i++) {
+    if (listaInvitati[i] == input) {
+        trovato=true;
+        break; //uscita anticipata dal loop
+    }
+}
+
+if(trovato) {
+    alert("Benvenuto" + listaInvitati[i]);
+}else {
+    //questo è il caso in cui non ho trovato l'utente
+    alert("Non è in lista");
+    }
