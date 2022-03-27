653082efbaad30f561b5980477144dba6ec1d59d
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-27 18:37:42 +0200

    jsnack6 setup

---
+++ jsnack6/script.js
@@ -0,0 +1,15 @@
+/*Gate generare un numero random da 0 a 10 al computer, e chiedete
+all'utente di inserire un suo numero. Se il numero scelto dall'utente è
+uguale al numero del computer informate l'utente che ha vinto, altrimenti
+ha perso. 
+ 
+A questo punto fate giocare l'utente, fintanto che non vince la partita
+contro il PC :)
+
+generare randomNumber
+
+let prompt utente numero intero
+
+loop while
+if a = b vinto, else rifai 
+*/
